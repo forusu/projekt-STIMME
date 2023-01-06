10da34ce4b3bb7040d23b24e4d96314217278ee9
isakkr <isakkr@sbb.rs>
2023-01-06 01:05:06 +0100

    Optimised keypad

--- index.js
+++ index.js
@@ -8,48 +8,16 @@ function playAudio(a){
     }
 
 
-function one() { 
-    array.push('1'); 
+function keypad(n) { 
+    array.push(n); 
 } 
-function two() { 
-    array.push('2'); 
-} 
-function three() { 
-    array.push('3'); 
-} 
-function four() { 
-    array.push('4'); 
-} 
-function five() { 
-    array.push('5'); 
-} 
-function six() { 
-    array.push('6'); 
-} 
-function seven() { 
-    array.push('7'); 
-} 
-function eight() { 
-    array.push('8'); 
-} 
-function nine() { 
-    array.push('9'); 
-} 
-function achtung() {
-    array.push('A');
-}
-function trennung() {
-    array.push('T');
-}
-function ende() {
-    array.push('E');
-}
 function clear() {
     array = []
 }
     
 function numbers() {
-
+    
+    console.log(array);
         let numbers = array.join("").split('')
     
         for (let i = 0; i < numbers.length; i++) {
@@ -77,7 +45,3 @@ function numbers() {
             
             
 }
-    
-    
-   
-
