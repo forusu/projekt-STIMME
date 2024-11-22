24e05727510f0a63b0fbbd536b3880b13975d42c
fsq <27868690+forusu@users.noreply.github.com>
2024-11-22 23:29:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let array = []
 let isPlaying = false
-let arrPos = 0
+let mode = "SPEECH"
 
 function playAudio(a){
 
@@ -17,27 +17,42 @@ function backward() {
     arrPos--
 }
 
-function keypad(n) { 
-    array.push(n); 
+function keypad(n) {
+    document.title = mode + "...." + n 
+    array.push(n);
+
 } 
+
+function clear() {
+    document.title = "CLEAR.."
+    array.splice(0, array.length)
+    numbers.splice(0, numbers.length)
+}
+
 function reset() {
+    document.title = "INPUT"
     array.splice(0, array.length)
     numbers.splice(0, numbers.length)
 }
     
 function numbers() {
-
+    document.title = "END"
+    let arrPos = 0
+    isPlaying = !isPlaying
     let numbers = array.join("").split('')
+
     if (arrPos > numbers.length)
-        arrPos = 0
-    for (let i = arrPos; i < numbers.length; i++) {
-    
-    (function (i) {
-        setTimeout(function () {
-            playAudio(numbers[i]);
-        }, 700 * i);
-    })(i);
-}
-            
+            arrPos = 0
+
+            for (let i = arrPos; i < numbers.length; i++) {
+                (function (i) {
+                    setTimeout(function () {
+                        if (isPlaying == true) {
+                            playAudio(numbers[i]);
+                            document.title = "o........." + numbers[i]           
+                        } 
+                    }, 700 * i);
+                })(i);     
+            }
             
 }
